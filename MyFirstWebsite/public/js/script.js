62c251630919b0aa1630886f2bc1316cdc70eadd
Itaru Kotagiri <harunoyuki1977@gmail.com>
2019-04-03 21:06:36 +0900

    記事アップと時間による背景色変更を実装

--- MyFirstWebsite/public/js/script.js
+++ MyFirstWebsite/public/js/script.js
@@ -1,10 +1,24 @@
 
-//index.phpの新着情報への書き込み文章
-$(function() {
-    $('.newinfo-text').html('2019.4.2 「PHP学習済み内容の吐き出し部屋」へ記事を1点追加しました。');
-});
 
-//index.phpのリリースノートへの書き込み文章
 $(function() {
-    $('.release-text').html('2019.4.2 新着情報の文章もjQuery(別ファイル)からの表示に変えてみました。');
+
+    //index.phpの新着情報への書き込み文章
+    $('.newinfo-text').html('2019.4.3 「Twitter風」の記事へ追記をしました。');
+
+    //index.phpのリリースノートへの書き込み文章
+    $('.release-text').html('2019.4.3 トップページ(本ページ)の背景色を時間により変更する様にしました。(jQuery)');
+
+    //時間帯によって色を変える
+
+    const d = new Date();
+    const hour = d.getHours();
+
+    if(hour >= 3 && hour < 9){
+        $('.index-body').css('background-color','aliceblue');
+    } else if(hour>= 10 && hour < 18){
+        $('.index-body').css('background-color','lightyellow');
+    } else {
+        $('.index-body').css('background-color','darkgray');
+    }
+
 });
