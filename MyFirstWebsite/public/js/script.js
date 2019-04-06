83dbeea12b236da9f5af7da6db5e3522eb2df22f
Itaru Kotagiri <harunoyuki1977@gmail.com>
2019-04-06 23:50:27 +0900

    CIKIをupなど

--- MyFirstWebsite/public/js/script.js
+++ MyFirstWebsite/public/js/script.js
@@ -3,10 +3,11 @@
 $(function() {
 
     //index.phpの新着情報への書き込み文章
-    $('.newinfo-text').html('2019.4.5 「今のお天気なあに？」を追加しました。');
+    $('.newinfo-text').html('2019.4.6 Programming noteへ「今のお天気なあに？」を改良しました。<br>2019.4.6 Programming noteへ「SQL学習済み内容の吐き出し部屋」の(書きかけ)記事へ追記しました。<br>2019.4.6 Music noteへ「BOKE」〜CIKI〜を追加しました。');
+
 
     //index.phpのリリースノートへの書き込み文章
-    $('.release-text').html('2019.4.5 「今のお天気なあに？」を追加しました。(PHP&jQuery&MySQL)');
+    $('.release-text').html('2019.4.6 「今のお天気なあに？」を改良しました。(PHP&jQuery&MySQL)');
 
     //時間帯によって色を変える
 
